576e7505d8768b602349c178df1facad8253e0d7
Jakub-Venglar <jakub.venglar@seznam.cz>
2023-07-07 13:17:48 +0200

    úprava logiky pro kouskování a posílání, příprava na tokenizaci

--- gpt-api-script.py
+++ gpt-api-script.py
@@ -60,62 +60,113 @@ def num_tokens_from_string(string: str) -> int:
 with open(file_name, 'r', encoding="utf-8") as file:
     text = file.read()
 
-paragraphs = []
-for line in text.split("\n"):
-    if len(line) > 0:
-        paragraphs.append(line)
+paragraphs_raw = []
+
+for line in text.split("\n"): # přidáme vše do seznamu po jednotlivých řádcích
+    paragraphs_raw.append(line)
+
+paragraphs= []
+skip = []
+
+# příprava textu - pokud je moc krátký, nebudeme jej posílat do GPT, ale zkusíme před tím přidat další odstavec
+#zároveň kontrolujeme, zda na řádku není něco, co chceme přeskakovat nebo nahrazovat
+#co nechceme poslat do GPT je označeno jako False nebo 'kratky' na začátku
+
+for i in range(len(paragraphs_raw)):
+    if len(paragraphs_raw[i])<1:
+        pass
+    if i in skip:
+        continue
+    if any(substring in paragraphs_raw[i] for substring in list_to_exclude): #kontroluje, zda v textu je nějaký řetězec, který jej diskvalifikuje z odeslání do GPT
+        for substring2 in list_to_exclude:
+            if substring2 in paragraphs_raw[i]:
+                if include_replacement == True and substring2 in replacements: #pokud máme nastavenou náhradu za takový kousek textu, tak ji aplikuje, jinak dá originální znění
+                    inter_text = replacements[substring2]
+                else:
+                    inter_text = (paragraphs_raw[i])
+                paragraphs.append([False,inter_text])
+
+    elif len(paragraphs_raw[i])<too_short: #pokud je odstavec moc krátký, GPT by to zmátlo, tak zkusíme přidat další nebo jej pouze zkopírujeme
+        temporary_text = paragraphs_raw[i] #základem je text z aktuálního odstavce
+        looper = 0
+        for j in range(i+1,len(paragraphs_raw)):
+            if any(substring in paragraphs_raw[j] for substring in list_to_exclude): #kontrolujeme následující odstavec na vyloučené výrazy
+                if len(temporary_text) < too_short: # zároveň je zatím poskládaný text moc krátký, dáme odpovídající příznak
+                    paragraphs.append(['kratky',temporary_text])
+                else: 
+                    paragraphs.append([True,temporary_text]) #prozatimní text je ok dlouhý, zapíšeme ho a ujistíme se, že se nezapíše dvakrát
+                
+                to_add = False
+                break
+            
+            else:
+                temporary_text = temporary_text + '\n' + paragraphs_raw[j+looper] #nic nebrání přidat další kousek textu
+                to_add = True
+                skip.append(j+looper)
+                looper += 1
+        
+        if to_add == True:
+            paragraphs.append([True,temporary_text])
+        
+
+    else:
+        paragraphs.append([True,paragraphs_raw[i]])
 
 generated_text = ""
+
+with open('pokus.txt', 'a', encoding='utf-8') as file: # na závěr smyčky přidá text na konec souboru s výsledkem, postupně tak přidává jednotlivé odstavce
+    file.write(str(paragraphs))
 
 print('Naporcováno a připraveno k magii...' + '\n')
 
 #smyčka, která rozkouskovaný text postupně dávkuje funkci pro zpracování v GPT
-'''
+
 for i in range(len(paragraphs)):
-    if any(substring in paragraphs[i] for substring in list_to_exclude): #kontroluje, zda v textu je nějaký řetězec, který jej diskvalifikuje z odeslání do GPT
-        for substring2 in list_to_exclude:
-            if substring2 in paragraphs[i]:
-                if include_replacement == True and substring2 in replacements: #pokud máme nastavenou náhradu za takový kousek textu, tak ji aplikuje, jinak dá originální znění
-                    inter_text = replacements[substring2] + '\n\n'
-                else:
-                    inter_text = (paragraphs[i] + '\n\n')
-                print(inter_text)
-                generated_text = inter_text
+    if paragraphs[i][0] == 'kratky': 
+        
+        #pokud je odstavec moc krátký, GPT by to zmátlo, tak jej jen zkopírujeme
+        
+        print('Text je hodně krátký. Vložím raději originál:' + '\n')
+        print(paragraphs[i][1] + '\n\n')
+        generated_text = (paragraphs[i][1] + '\n\n')
         gpt_processed = False
 
-    elif len(paragraphs[i])<too_short: #pokud je odstavec moc krátký, GPT by to zmátlo, tak jej jen zkopírujeme
-            print('Text je hodně krátký. Vložím raději originál:' + '\n')
-            print(paragraphs[i] + '\n\n')
-            generated_text = (paragraphs[i] + '\n\n')
-            gpt_processed = False
+    elif paragraphs[i][0] == False: 
+        
+        #kontroluje, zda v textu je nějaký řetězec, který jej diskvalifikuje z odeslání do GPT
+        #pokud máme nastavenou náhradu za takový kousek textu, tak ji aplikuje, jinak dá originální znění
+        
+        print('Tohle nechceme posílat, vkládám co je připraveno nebo originál:' + '\n')
+        print(paragraphs[i][1] + '\n\n')
 
-    else:
+        generated_text = (paragraphs[i][1] + '\n\n')
+        gpt_processed = False
+
+    elif paragraphs[i][0] == True:
         print('Předávám text do GPT...' + '\n') #předání textu do GPT
         t = time.time()
-        gpt_text = generate_text_from_paragraphs(paragraphs[i], prompt) + '\n\n'
+        gpt_text = generate_text_from_paragraphs(paragraphs[i][1], prompt) + '\n\n'
         print('GPT: ' + gpt_text)
         gpt_processed = True
 
         if any(correct_info in gpt_text for correct_info in gpt_annoucements_to_exclude): #pokud GPT vyplivnul nějaké moudro, které máme nastaveno, že nechceme slyšet, tak raději vloží originální text
-            print('Vložím raději originál:' + '\n')
-            print(paragraphs[i] + '\n')
-            generated_text = (paragraphs[i] + '\n\n')
+            print('Prý' + gpt_text + '\n' + '...' + '\n' 'Vložím raději originál:' + '\n')
+            print(paragraphs[i][1] + '\n')
+            generated_text = (paragraphs[i][1] + '\n\n')
 
         else:
-            generated_text = (gpt_text + '\n\n')
+            generated_text = gpt_text
 
     if gpt_processed == True: #pokud jsme text poslali do GPT, zkontrolujeme rate limits
 
         t2 = time.time() # nejprve pro čas
         time_between_loops = t2-t
         if time_between_loops < necessary_delay:
-            delay = necessary_delay-time_between_loops
+            delay = necessary_delay - time_between_loops
             time.sleep(delay)
     
 
     with open(result_name, 'a', encoding='utf-8') as file: # na závěr smyčky přidá text na konec souboru s výsledkem, postupně tak přidává jednotlivé odstavce
                 file.write(generated_text)
-
-                '''
 
 print('***HOTOVO***') #je li vše zpracováno, dá nám vědět
